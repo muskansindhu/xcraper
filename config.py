77a7f4f29105bd3d89622857faa600b68b51c8d3
muskansindhu <shunyamuskan@gmail.com>
2025-01-23 18:47:16 +0530

    feat(xcraper): add check rate limit method and refactor search api

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ class Config:
     BEARER_TOKEN = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs=1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
     USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
     REFERER = "https://twitter.com/"
+    GQL_URL = "https://x.com/i/api/graphql"
--- xcraper.py
+++ xcraper.py
@@ -3,12 +3,15 @@ import re
 import asyncio
 from typing import Any, Dict, List, Tuple, Union
 from httpx import AsyncClient, Headers
-
-from utils import find_key, encode_params
+import time
+import json
 from dotenv import load_dotenv
+
+from utils import find_key, encode_params, find_obj
 from constants import GQL_FEATURES
 from config import Config
 from account import Account
+from models import parse_tweets
 
 load_dotenv()
 
@@ -20,27 +23,12 @@ proxy = os.getenv("PROXY")
 class Xcraper:
     """A class for scraping data using Twitter's GraphQL API."""
 
-    def get_cursor(self, data: Union[List[Any], Dict[str, Any]]) -> str:
-        """Extracts the cursor from the provided data.
+    def _get_cursor(self, obj: dict, cursor_type="Bottom") -> str | None:
+        if cur := find_obj(obj, lambda x: x.get("cursorType") == cursor_type):
+            return cur.get("value")
+        return None
 
-        Args:
-            data (Union[List[Any], Dict[str, Any]]): The response data to search for cursors.
-
-        Returns:
-            str: The extracted cursor value, if found.
-        """
-        entries = find_key(data, 'entries')
-        if entries:
-            for entry in entries.pop():
-                entry_id = entry.get('entryId', '')
-                if 'cursor-bottom' in entry_id or 'cursor-showmorethreads' in entry_id:
-                    content = entry['content']
-                    if item_content := content.get('itemContent'):
-                        return item_content['value']  # v2 cursor
-                    return content['value']  # v1 cursor
-        return ""
-
-    def get_response_headers(self, headers: Headers) -> Dict[str, str]:
+    def _get_response_headers(self, headers: Headers) -> Dict[str, str]:
         """Extracts rate limit headers from the response.
 
         Args:
@@ -51,16 +39,35 @@ class Xcraper:
         """
         header_keys = ['x-rate-limit-limit', 'x-rate-limit-remaining', 'x-rate-limit-reset']
         return {key: headers.get(key, '') for key in header_keys}
+    
+    def _check_rate_limit(self, header_dict: dict) -> bool:
+        rate_limit = int(header_dict['x-rate-limit-limit'])
+        rem_rate_limit = int(header_dict['x-rate-limit-remaining'])
 
-    async def search(self, client: AsyncClient, query: str) -> Tuple[Dict[str, Any], List[Dict[str, Any]], str, Dict[str, str]]:
-        """Performs a search query using Twitter's GraphQL API.
+        if rem_rate_limit < 0.3 * rate_limit:
+            return False 
+        
+        return True
+
+    async def fetch_search_page(
+        self,
+        client: AsyncClient,
+        query: str,
+        cursor: str | None,
+        features: dict,
+        cursor_type: str = "Bottom"
+    ) -> Tuple[Dict[str, Any], List[Dict[str, Any]], str, Dict[str, str]]:
+        """Fetches a single page of search results using Twitter's GraphQL API.
 
         Args:
             client (AsyncClient): The HTTPX asynchronous client.
             query (str): The search query string.
+            cursor (str | None): The cursor for pagination.
+            features (dict): GraphQL features.
+            cursor_type (str): The type of cursor to use for pagination.
 
         Returns:
-            Tuple[Dict[str, Any], List[Dict[str, Any]], str, Dict[str, str]]: The response data, extracted entries, cursor, and response headers.
+            Tuple[Dict[str, Any], List[Dict[str, Any]], str, Dict[str, str]]: The response data, entries, next cursor, and response headers.
         """
         # Prepare query variables
         variables = {
@@ -69,41 +76,84 @@ class Xcraper:
             "product": "Latest",
             "querySource": "typed_query",
         }
+        if cursor:
+            variables["cursor"] = cursor
 
         # Construct request parameters
         params = {
             "variables": variables,
-            "features": {**GQL_FEATURES},
+            "features": features,
             "fieldToggles": {"withArticleRichContentState": False},
         }
 
         # Define GraphQL endpoint details
-        qid = 'nK1dw4oV3k4w5TdtcAdSww'
-        name = 'SearchTimeline'
+        qid = "nK1dw4oV3k4w5TdtcAdSww"
+        name = "SearchTimeline"
 
         # Send GET request
         response = await client.get(
-            f'https://twitter.com/i/api/graphql/{qid}/{name}',
+            f"{Config.GQL_URL}/{qid}/{name}",
             params=encode_params(params)
         )
+        if response is None:
+            return {}, [], None, {}
 
         # Parse response
         data = response.json()
-        cursor = self.get_cursor(data)
         entries = [
-            entry for entry_list in find_key(data, 'entries')
+            entry for entry_list in find_key(data, "entries")
             for entry in entry_list
-            if re.search(r'^(tweet|user)-', entry['entryId'])
+            if re.search(r"^(tweet|user)-", entry["entryId"])
         ]
-
-        # Extract response headers
-        response_headers = self.get_response_headers(response.headers)
 
         # Add query to each entry
         for entry in entries:
-            entry['query'] = variables['rawQuery']
+            entry["query"] = variables["rawQuery"]
 
-        return data, entries, cursor, response_headers
+        next_cursor = self._get_cursor(data, cursor_type)
+        response_headers = self._get_response_headers(response.headers)
+
+        return data, entries, next_cursor, response_headers
+
+    async def search(
+        self,
+        client: AsyncClient,
+        query: str,
+        cursor_type: str = "Bottom"
+    ):
+        """Performs a paginated search query using Twitter's GraphQL API.
+
+        Args:
+            client (AsyncClient): The HTTPX asynchronous client.
+            query (str): The search query string.
+            limit (int): The maximum number of results to fetch (-1 for no limit).
+            cursor_type (str): The type of cursor to use for pagination.
+
+        Yields:
+            Tuple[Dict[str, Any], List[Dict[str, Any]], str, Dict[str, str]]: The response data, entries, cursor, and response headers.
+        """
+        features = {**GQL_FEATURES}
+        cur, cnt, active = None, 0, True
+
+        while active:
+            await asyncio.sleep(3)
+            data, entries, next_cursor, response_headers = await self.fetch_search_page(
+                client, query, cur, features, cursor_type
+            )
+
+            if not entries:
+                break
+
+            yield parse_tweets(data), entries, next_cursor, response_headers
+
+            # Update variables for pagination
+            cnt += len(entries)
+            if not self._check_rate_limit(response_headers):
+                active = False
+            # elif not next_cursor:
+            #     active = False
+
+            cur = next_cursor
 
 async def main():
     xcraper = Xcraper()
@@ -114,13 +164,33 @@ async def main():
 
     query = "elon musk"
 
-    data, entries, cursor, response_headers = await xcraper.search(client, query)
+    try:
+        tweets = []
+        async for data, entries, cursor, response_headers in xcraper.search(client, query):
+            print(response_headers)
 
-    print(response_headers)
+            if not xcraper._check_rate_limit(response_headers):
+                print("Rate limit reached. Exiting loop.")
+                break
+
+            
+            for tweet in data:
+                tweets.append({
+                    "id": tweet.id,
+                    "url": tweet.url,
+                    "text": tweet.rawContent
+                })
+
+            print("writing in file")
+            with open(f"res.json", "w") as f:
+                json.dump(tweets, f, indent=2)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        await client.aclose()
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-    
-
-
